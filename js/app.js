e7f61b52074032ce99d574441b5393a95dabf1ab
abdur-rehman-ali <alijoyia911@gmail.com>
2021-03-11 22:19:51 +0500

    final

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('.nav-link');
-console.log(hamburger);
-console.log(nav);
-
 hamburger.addEventListener('click',()=>
 {
  
